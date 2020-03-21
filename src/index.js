246ac7c4c1378b2783516f1f88b0cf35d4c8f59d
Juan Polanco <juanpolanco5008@gmail.com>
2020-03-20 22:54:46 -0400

    added JS map for each card

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js'
+// import Card from './Card.js'
+import Cardlist from './Cardlist'
 // import App from './App';
 // import Hello from './Hello'
 import * as serviceWorker from './serviceWorker';
@@ -12,11 +13,7 @@ import { robots } from './robots';
 ReactDOM.render(
   <React.StrictMode>
     <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-
-
+    <Cardlist robots={robots}/>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
