110b93ef0707ff3dc301412900a46e75e0ea8f4c
Juan Polanco <juanpolanco5008@gmail.com>
2020-03-20 22:57:05 -0400

    added Ket Id for each card

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -4,7 +4,7 @@ import Card from './Card';
 
 const Cardlist = ({ robots }) => {
 	const cardComponent = robots.map((user, i) => {
-		return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+		return <Card key={i}id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
 	})
 	return (
 
