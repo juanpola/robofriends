e23b740390c0e1a425b0dcf837b2492c7743af1d
Juan Polanco <juanpolanco5008@gmail.com>
2020-03-23 22:12:37 -0400

    added static search bar when user is scrolling!

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Cardlist from './Cardlist';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll'
 
 
 //state is something that can be changed, It ussually lives in the parent component
-
+//smart component
 class App extends React.Component {
-
-
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> {
+			return response.json();
+		})
+		.then(users => {
+			this.setState({ robots: users})
+		});
 	}
 
 	onSearchChange = (event) => {
@@ -27,13 +36,19 @@ class App extends React.Component {
 		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
 		console.log(filterRobots);
+		if (robots.length === 0 ){
+			return <h1> Loading</h1>
+		} else {
 	return (
 		<div className='tc'>
 		<h1 className='f1'> RoboFriends </h1>
 		<SearchBox searchChange={this.onSearchChange}/>
+			<Scroll>
 			<Cardlist robots={filterRobots}/>
+			</Scroll>
 		</div>
 		);
+}
 }
 }
 
